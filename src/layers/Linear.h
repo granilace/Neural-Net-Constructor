30f828031acb2402768708360cd97bf6609409a0
Victor Kukanov <vakukanov@edu.hse.ru>
2021-03-20 14:16:51 +0300

    add Linear layer

--- src/layers/Linear.h
+++ src/layers/Linear.h
@@ -1,21 +1,24 @@
-//
-// Created by Sergei Troshin on 18.03.2021.
-//
+#pragma once
 
-#ifndef NEURAL_NET_CONSTRUCTOR1_LINEAR_H
-#define NEURAL_NET_CONSTRUCTOR1_LINEAR_H
 #include <vector>
 #include "Layer.h"
 
 template<typename T>
 class Linear : public Layer<T> {
 public:
-    explicit Linear() {};
+    Linear(size_t input_size, size_t output_size): weights_(NormTensor<T>(input_size, output_size)), bias_(NormTensor<T>(output_size, 1)) {
+    }
 
-    Tensor<T> forward(Tensor<T> const &X) override {};
+    Tensor<T> forward(Tensor<T> const &X) override {
+        return (X * weights_).rowwise() + bias_.rowwise().sum().transpose();
+    };
 
-    Tensor<T> backward(Tensor<T> const &grad) override {};
+    Tensor<T> backward(Tensor<T> const &grad) override {
+        return grad * weights_.transpose();
+    };
 
+private:
+    Tensor<T> weights_;
+    Tensor<T> bias_;
 };
 
-#endif //NEURAL_NET_CONSTRUCTOR1_LINEAR_H
--- test/layers.cpp
+++ test/layers.cpp
@@ -3,7 +3,10 @@
 #include "utils/Tensor.h"
 #include "utils/Parameter.h"
 #include "layers/Identity.h"
+#include "layers/Linear.h"
 #include "layers/Sequential.h"
+
+static const float kEps = 1e-7;
 
 TEST(Sequential, Identity) {
     Tensor<float> tensor(3, 4);
@@ -21,5 +24,37 @@ TEST(Sequential, Identity) {
     });
     Tensor<float> loss = model.forward(input);
     model.backward(loss);
-    ASSERT_NEAR(loss.sum(), 12.0, 1e-7);
+    ASSERT_NEAR(loss.sum(), 12.0, kEps);
+}
+
+TEST(Tmp, tmp) {
+    Eigen::Matrix<float, 10, 4> input;
+    input.setOnes();
+
+    Eigen::Matrix<float, 4, 3> weight;
+    weight.setOnes();
+
+    Eigen::Matrix<float, 3, 1> bias;
+    bias.setOnes();
+
+    Eigen::VectorXf vector_bias(3);
+
+    auto mul_result = input * weight;
+    std::cout << mul_result.rows() << " " << mul_result.cols() << std::endl;
+    auto bias_sum_result = bias.rowwise().sum();
+    std::cout << bias_sum_result.rows() << " " << bias_sum_result.cols() << std::endl;
+    auto tmp = mul_result.rowwise() + bias.transpose();
+    std::cout << tmp.rows() << " " << tmp.cols() << std::endl;
+    std::cout << tmp << std::endl;
+}
+
+TEST(Linear, Forward) {
+    Linear<float> linear(4, 3);
+    Tensor<float> input(10, 4);
+    input.setOnes();
+    auto output = linear.forward(input);
+    auto output_row_sum = output.rowwise().sum();
+    ASSERT_EQ(output.rows(), 10);
+    ASSERT_EQ(output.cols(), 3);
+    ASSERT_NEAR(output_row_sum.minCoeff(), output_row_sum.maxCoeff(), kEps); // check that all output rows are the same
 }
